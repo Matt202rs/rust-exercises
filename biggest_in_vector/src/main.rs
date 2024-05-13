6dda0a36424125dc02432111325d8f1f4cc79344
Matheus Rosa <matheus202rs@gmail.com>
2024-05-13 10:49:43 -0300

    improved type reading

--- biggest_in_vector/src/main.rs
+++ biggest_in_vector/src/main.rs
@@ -1,15 +1,15 @@
 fn biggest(vector: &[i32]) -> i32 {
-    let mut biggest = vector[0];
+    let mut biggest: i32 = vector[0];
     for i in vector {
-        if i > biggest {
-            biggest = i;
+        if i > &biggest {
+            biggest = *i;
         }
     }
     return biggest;
 }
 
 fn main() {
-    let vector = vec![1, 12, 35, 94, 75];
-    let biggest = biggest(vector);
+    let vector: Vec<i32> = vec![1, 12, 35, 94, 75];
+    let biggest: i32 = biggest(&vector);
     println!("{}", biggest);
 }
